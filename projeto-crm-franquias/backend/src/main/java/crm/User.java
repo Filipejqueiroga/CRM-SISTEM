3544057b2a17accbe3ab94f37ee93d47771a5d43
Filipejqueiroga <filipejqueiroga@gmail.com>
2025-09-12 09:17:43 -0300

    Files and initial user code

---
+++ projeto-crm-franquias/backend/src/main/java/crm/User.java
@@ -0,0 +1,12 @@
+public class User {
+    String email;
+    String user_name;
+    String password;
+
+    User (String email, String user_name, String password){
+        this.email = email;
+        this.user_name = user_name;
+        this.password = password;
+    }
+}
+
