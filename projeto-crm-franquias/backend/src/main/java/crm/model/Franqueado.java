f050218699ae16008849ff69f98ae9b187aaa47d
rebecabelmont <rebeca.belmont777@gmail.com>
2025-09-18 23:30:09 -0300

    resolvendo conflitos com o remoto

--- projeto-crm-franquias/backend/src/main/java/crm/model/Franqueado.java
+++ projeto-crm-franquias/backend/src/main/java/crm/model/Franqueado.java
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-package src.main.java.crm.model;
-
-=======
->>>>>>> develop
 public class Franqueado extends Usuario {
     private String nome_franquia;
 
--- projeto-crm-franquias/backend/src/main/java/crm/model/Franqueador.java
+++ projeto-crm-franquias/backend/src/main/java/crm/model/Franqueador.java
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-package src.main.java.crm.model;
-
-=======
->>>>>>> develop
 public class Franqueador extends Usuario {
     
     private String nome_empresa;
